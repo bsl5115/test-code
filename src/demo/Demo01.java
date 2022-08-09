686512895e64b8e8f1b8a1b336d4fc6c3540160d
yuanshan <bsl5115@163.com>
2022-08-09 23:19:57 +0800

    test-commit

---
+++ src/demo/Demo01.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * @author
+ * @date 2022/8/9 23:00
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
